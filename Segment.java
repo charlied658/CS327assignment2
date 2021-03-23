423f454ea550ca6929e3146b59f7afc03857e74f
charlied658 <80932655+charlied658@users.noreply.github.com>
2021-03-23 12:32:50 -0400

    Update Segment.java

--- Segment.java
+++ Segment.java
@@ -8,12 +8,6 @@ public class Segment implements java.io.Serializable{
   int checksum;
   char data[];
 
-  final static int SYN = 0;
-  final static int SYNACK = 1;
-  final static int FIN = 2;
-  final static int FINACK = 3;
-  final static int DATA = 4;
-  final static int DATAACK = 5;
 
   public Segment(int whatType){
     if(whatType < 0 || whatType > 5){
